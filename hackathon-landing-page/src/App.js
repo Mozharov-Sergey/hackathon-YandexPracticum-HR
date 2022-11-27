d36f801848667499e5113fcab8187f5e40b5c6cf
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-11-27 12:58:53 +0300

    feature: Добавить шрифты YS

--- hackathon-landing-page/src/App.js
+++ hackathon-landing-page/src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Courses from './components/Courses';
 
 function App() {
   return (
     <div className="root">
       <div className="page">
-        <Header></Header>
+        {/* <Header></Header> */}
+        <Courses></Courses>
         <Footer></Footer>
       </div>
     </div>
---
+++ hackathon-landing-page/src/components/Courses.js
@@ -0,0 +1,12 @@
+export default function Courses() {
+return(<>
+  <section className="courses">
+    <h2 className="courses__title">Чему мы учим</h2>
+    <p className="courses__main-text">Студенты Яндекс Практикума учатся на 5 основных факультетах. И им очень нехватает вашего профессионального опыта</p>
+    <div className="courses___cards-container">
+      <div className="courses__card"></div>
+      <div className="courses__final-card"></div>
+    </div>
+  </section>
+</>)
+}
