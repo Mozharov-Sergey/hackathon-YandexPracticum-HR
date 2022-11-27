dceef9453a5829c08403823eab46ba4ecd8d9910
Maksim Nikolaev <mksniklaev@gmail.com>
2022-11-27 20:17:26 +0500

    feat: add section Reviews Job and Footer

--- hackathon-landing-page/src/App.js
+++ hackathon-landing-page/src/App.js
@@ -1,12 +1,16 @@
+import Footer from './components/Footer/Footer';
 import Header from './components/Header';
-import Footer from './components/Footer';
+import Job from './components/Job/Job';
+import Reviews from './components/Reviews/Reviews';
 
 function App() {
   return (
     <div className="root">
       <div className="page">
         <Header></Header>
-        <Footer></Footer>
+        <Reviews />
+        <Job />
+        <Footer />
       </div>
     </div>
   );
