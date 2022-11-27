83fb911f2c2425f9b388f415f679fbf22d48c96c
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-11-27 20:08:46 +0300

    refactor: магия мерджа

--- hackathon-landing-page/src/App.js
+++ hackathon-landing-page/src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="root">
       <div className="page">
+        <Header></Header>
         <Courses></Courses>
         <Terms></Terms>
         <HighlightOffers></HighlightOffers>
