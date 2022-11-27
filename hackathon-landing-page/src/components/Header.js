6cb4082e8a4ba0cc7a393240b6d23180ae55eec2
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-11-27 14:41:38 +0300

    header

--- hackathon-landing-page/src/components/Header.js
+++ hackathon-landing-page/src/components/Header.js
@@ -1,10 +1,23 @@
+import headerLogo from '../images/headerLogo.svg';
+
 export default function Header() {
   return(
     <>
-    <h1>ПОГНАЛИ!!!!!!!!!</h1>
-    <h2>Наши городских</h2>
-    <h4>Наши впереди</h4>
-    <h6>Городские сзади</h6>
+      <header class="header">
+      <a href="https://practicum.yandex.ru/" className="header__logo">
+        <img src={headerLogo} alt="Панда"></img></a>
+      <nav className="header__navigation">
+      <ul className="header__links">
+        <li><a href="https://practicum.yandex.ru/" className="header_link">Задачи</a></li>
+        <li><a href="https://practicum.yandex.ru/" className="header_link">Факультеты</a></li>
+        <li><a href="https://practicum.yandex.ru/" className="header_link">Условия</a></li>
+        <li><a href="https://practicum.yandex.ru/" className="header_link">Вакансии</a></li>
+        <li><a href="https://practicum.yandex.ru/" className="header_link">Отзывы</a></li>
+        <li><a href="https://practicum.yandex.ru/" className="header_link">Контакты</a></li>
+      </ul>
+      </nav>
+      <button className="header_button">Начать учить</button>
+      </header>
     </>
   )
 }
