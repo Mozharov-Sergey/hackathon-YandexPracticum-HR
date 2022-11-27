d42c3c841552c03e17ba43e2524bcb4a857aa5b2
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-11-27 19:48:27 +0300

    блок3

--- hackathon-landing-page/src/components/Instructions.js
+++ hackathon-landing-page/src/components/Instructions.js
@@ -1,31 +1,42 @@
-import testImage from '../images/testImage.png'
-
+import testImage from '../images/instructions/testImage.png'
+import arrowSecong from '../images/instructions/arrowSecong.png'
+import arrowFirst from '../images/instructions/arrowFirst.png'
 export default function Instructions() {
   return(<>
 <section className="instructions">
   <h2 class="instructions__title">Что нужно будет делать</h2>
-  <div className="mentors">
-    <h3 class="mentors__title instructions__subtitle">Наставникам:</h3>
-      <ul class="mentors__tasks instructions__tasks">
-        <li class="mentors__task">Помогать студентам ставить цели, рефлексировать
-            и делать выводы Отвечать на их вопросы в чате</li>
-        <li class="mentors__task instruction__task">Проводить вебинары 2 раза в месяц и давать обратную связь</li>
-        <li class="mentors__task instruction__task">Делиться опытом и мотивировать</li>
+  <div className="instructions__mentors">
+    <h3 className="instructions__title_mentors instructions__subtitle">Наставникам:</h3>
+      <ul className="instructions__tasks">
+        <li className="instructions__task">Помогать студентам ставить цели, рефлексировать
+        и&nbsp;делать выводы</li>
+        <li className="instructions__task">Отвечать на их вопросы в чате</li>
+        <li className="instructions__task">Проводить вебинары 2 раза в месяц и давать обратную&nbsp;связь</li>
+        <li className="instructions__task">Делиться опытом и мотивировать</li>
       </ul>
-    <img class="instructions__image" src={testImage} alt="картинка"></img>
-    <button className="instructions__button"></button>
+    <img className="instructions__image" src={testImage} alt="картинка"></img>
+    <button className="instructions__button">Буду наставником</button>
   </div>
-  {/* <div className="reviewers">
-    <h3 class="reviewers__title instructions__subtitle">Наставникам:</h3>
-      <ul class="reviewers__tasks instructions__tasks">
-        <li class="reviewers__task instruction__task">Помогать студентам ставить цели, рефлексировать
-            и делать выводы Отвечать на их вопросы в чате</li>
-        <li class="reviewers__task instruction__task">Проводить вебинары 2 раза в месяц и давать обратную связь</li>
-        <li class="reviewers__task instruction__task">Делиться опытом и мотивировать</li>
+  <div className="instructions__reviewers">
+    <h3 className="instructions__title_reviewers instructions__subtitle">Ревьюерам:</h3>
+      <ul className="instructions__tasks">
+        <li className="instructions__task">Проверять код и проекты студентов</li>
+        <li className="instructions__task">Простыми словами объяснять им их ошибки</li>
+        <li className="instructions__task">Давать корректирующую обратную связь</li>
+        <li className="instructions__task">Оценивать работы в формате зачёт/незачёт</li>
       </ul>
-    <img class="mentors__image" src="./images/workout.jpg" alt="картинка"></img>
-    <button className="instructions__button"></button>
-  </div> */}
+      <img className="instructions__image" src={testImage} alt="картинка"></img>
+    <button className="instructions__button">Пойду в ревьюеры</button>
+  </div>
+  <div className="todo-list">
+    <p className="todo-list__text todo-list__text-first">Выберите роль и&nbsp;заполните заявку
+      <img className="todo-list__arrow-first"src={arrowFirst} alt="стрелка"></img>
+    </p>
+    <p className="todo-list__text todo-list__text-second">Пройдите отбор и&nbsp;бесплатное обучение
+      <img className="todo-list__arrow-secong"src={arrowSecong} alt="стрелка"></img>
+    </p>
+    <p className="todo-list__text"> Делитесь знаниями со&nbsp;cтудентами Яндекс Практикума</p>
+  </div>
 </section>
   </>)
   }
