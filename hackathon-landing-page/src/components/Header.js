44bc50f1520b03ca5de972884edca7717b646a8f
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-11-28 22:46:39 +0300

    блюр интро + шрифт, отсупы хедр, шрифт test

--- hackathon-landing-page/src/components/Header.js
+++ hackathon-landing-page/src/components/Header.js
@@ -5,15 +5,15 @@ export default function Header() {
   return(
       <header className="header">
       <a href="https://practicum.yandex.ru/" className="header__logo">
-        <img src={headerLogo} alt="Панда"></img></a>
+        <img src={headerLogo} alt="логотип яндекс-практикум"></img></a>
       <nav className="header__navigation">
       <ul className="header__links">
-        <li><a href="#tasks"  className="header_link">Задачи</a></li>
-        <li><a href="#courses" className="header_link">Факультеты</a></li>
-        <li><a href="#offers" className="header_link">Условия</a></li>
-        <li><a href="#job" className="header_link">Вакансии</a></li>
-        <li><a href="#reviews" className="header_link">Отзывы</a></li>
-        <li><a href="#footer" className="header_link">Контакты</a></li>
+        <li><a href="#tasks" className="header__link">Задачи</a></li>
+        <li><a href="#courses" className="header__link">Факультеты</a></li>
+        <li><a href="#offers" className="header__link">Условия</a></li>
+        <li><a href="#job" className="header__link">Вакансии</a></li>
+        <li><a href="#reviews" className="header__link">Отзывы</a></li>
+        <li><a href="#footer" className="header__link">Контакты</a></li>
       </ul>
       </nav>
       <Button text='Начать учить' width='140px'/>
