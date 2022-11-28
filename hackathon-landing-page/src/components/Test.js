2e0848eb84cf3ff52c58d56e0b60aab7e9272351
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-11-28 21:40:13 +0300

    поправила везде отсупы, поменяла шрифт блок test. поменяла текст в оодном блоке, картинки в инструкциях

--- hackathon-landing-page/src/components/Test.js
+++ hackathon-landing-page/src/components/Test.js
@@ -3,15 +3,13 @@ import cat from '../images/test/cat.png';
 
 export default function Test() {
   return(
-    <>
     <section className='test'>
         <img className="test__image-lines" src={lines} alt=""></img>
         <h4 className="test__title">Прирожденный наставник или истинный ревьюер?</h4>
-        <p className='test__paragraph'>Пройдите короткий тест под руководством нашего hr-специалиста:
+        <p className='test__paragraph'>Пройдите короткий тест под руководством нашего hr&nbsp;-&nbsp;специалиста:
           он поможет определиться с наиболее подходящей для вас ролью</p>
         <button className="test__button">Пройти тест</button>
       <img className="test__image-cat"src={cat} alt=""></img>
     </section>
-    </>
   )
 }
