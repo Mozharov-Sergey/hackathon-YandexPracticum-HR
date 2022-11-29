c4da93c72042d72a36a865d1f99c1888e0e83943
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-11-29 20:15:22 +0300

    feature: Добавить первый вопрос в бот

--- hackathon-landing-page/src/components/Test.js
+++ hackathon-landing-page/src/components/Test.js
@@ -1,16 +1,46 @@
-import lines from '../images/test/lines.png';
-import cat from '../images/test/cat.png';
-import Button from './Button/Button';
+import React from 'react';
+import TestStartPage from './TestStartPage';
+import TestInProgress from './TestInProgress';
 
 export default function Test() {
-  return(
-    <section className='test'>
-        <img className="test__image-lines" src={lines} alt=""></img>
-        <h4 className="test__title">Прирожденный наставник или истинный ревьюер?</h4>
-        <p className='test__paragraph'>Пройдите короткий тест под руководством нашего hr&nbsp;-&nbsp;специалиста:
-          он поможет определиться с наиболее подходящей для вас ролью</p>
-        <Button text='Пройти тест' width='185px'/>  
-      <img className="test__image-cat"src={cat} alt=""></img>
+  const dialogs = {
+    startQuestions: [
+      'Привет! Меня зовут Света, я сотрудник hr-команды:)',
+      'Если вам сложно определиться с тем, какая роль вам подходит, я готова помочь',
+      'Какой профиль вас интересует?',
+    ],
+    qualitiesQuestions: ['Выберите все качества, которые вам подходят:'],
+    capabilitiesQuestions: [
+      'Отлично! С качествами мы разобрались, теперь необходимо понять ваши возможности:',
+    ],
+    expressionsQuestions: ['Супер!, остался последний вопрос. Выбери 1 или 2 подходящих тебе выражения:'],
+
+    startAnswers: [
+      { name: 'programming', value: 'Программирование' },
+      { name: 'analitics', value: 'Анализ данных' },
+      { name: 'design', value: 'Дизайн' },
+      { name: 'management', value: 'Менеджмент' },
+      { name: 'marketing', value: 'Маркетинг' },
+    ],
+    qualitiesAnswers: [
+      { name: 'empatic', value: 'Я эмпатичный человек' },
+      { name: 'focus', value: 'Мне легко удерживать внимание людей' },
+      { name: 'contact', value: 'Я люблю общаться' },
+      { name: 'write', value: 'Мне легче писать, чем говорить' },
+      { name: 'feedback', value: 'Знаю принципы фидбэка' },
+    ]
+  };
+
+  const [isInProgress, setIsInProgress] = React.useState(false);
+
+  function handleStart() {
+    setIsInProgress(true);
+  }
+
+  return (
+    <section className="test">
+      {!isInProgress && <TestStartPage onStart={handleStart}></TestStartPage>}
+      {isInProgress && <TestInProgress messages={dialogs}></TestInProgress>}
     </section>
-  )
+  );
 }
---
+++ hackathon-landing-page/src/components/TestInProgress.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+export default function TestInProgress({ messages }) {
+  const [isCourseClicked, setIsCourseClicked] = React.useState(false);
+  const [chosenCourse, setChosenCourse] = React.useState('');
+  const [chosenQualities, setChosenQualities] = React.useState([]);
+  const answers = {};
+
+  function handleClickCourse(e) {
+    setIsCourseClicked(!isCourseClicked);
+    setChosenCourse(e.target.value);
+  }
+
+  function handleClickQualities(e) {
+    setChosenQualities([...chosenQualities, e.target.value])
+    e.target.className.add('test__answer_active');
+  }
+
+  function renderBotMessages(data) {
+    return data.map((item, index) => {
+      return (
+        <p className="from-yandex" key={index}>
+          {item}
+        </p>
+      );
+    });
+  }
+
+  React.useEffect(() => {
+    console.log(chosenQualities)
+  }, [chosenQualities])
+
+  return (
+    <>
+      {renderBotMessages(messages.startQuestions)}
+
+      {isCourseClicked ? (
+        <p className="from-applicant">{chosenCourse}</p>
+      ) : (
+        <div className="test__answer-container">
+          {messages.startAnswers.map((item) => {
+            return (
+              <button
+                name={item.name}
+                className={`test__answer`}
+                onClick={handleClickCourse}
+                value={item.value}
+              >
+                {item.value}
+              </button>
+            );
+          })}
+        </div>
+      )}
+
+      {isCourseClicked && renderBotMessages(messages.qualitiesQuestions)}
+
+      {isCourseClicked && (
+        <div className="test__answer-container">
+          {messages.qualitiesAnswers.map((item) => {
+            return (
+              <button
+                name={item.name}
+                className={`test__answer`}
+                value={item.value}
+                onClick={handleClickQualities}
+              >
+                {item.value}
+              </button>
+            );
+          })}
+        </div>
+      )}
+    </>
+  );
+}
