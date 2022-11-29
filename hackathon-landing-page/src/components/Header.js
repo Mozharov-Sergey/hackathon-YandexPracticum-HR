7e157cf83807b7827ba7fb0dc2106f2661079282
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2022-11-29 13:23:00 +0300

    заготовка бургера

--- hackathon-landing-page/src/components/Header.js
+++ hackathon-landing-page/src/components/Header.js
@@ -8,15 +8,21 @@ export default function Header() {
         <img src={headerLogo} alt="логотип яндекс-практикум"></img></a>
       <nav className="header__navigation">
       <ul className="header__links">
-        <li><a href="#tasks" className="header__link">Задачи</a></li>
-        <li><a href="#courses" className="header__link">Факультеты</a></li>
-        <li><a href="#offers" className="header__link">Условия</a></li>
-        <li><a href="#job" className="header__link">Вакансии</a></li>
-        <li><a href="#reviews" className="header__link">Отзывы</a></li>
-        <li><a href="#footer" className="header__link">Контакты</a></li>
+        <li className='header__link-burger'><a href="#tasks" className="header__link">Задачи</a></li>
+        <li className='header__link-burger'><a href="#courses" className="header__link">Факультеты</a></li>
+        <li className='header__link-burger'><a href="#offers" className="header__link">Условия</a></li>
+        <li className='header__link-burger'><a href="#job" className="header__link">Вакансии</a></li>
+        <li className='header__link-burger'><a href="#reviews" className="header__link">Отзывы</a></li>
+        <li className='header__link-burger'><a href="#footer" className="header__link">Контакты</a></li>
       </ul>
       </nav>
-      <Button text='Начать учить' width='140px'/>
+      <div className='header__buttons'>
+        <button className='header__burger'></button>
+        <div className='header__button'>
+          <Button text='Начать учить' width='140px'/>
+        </div>
+      </div>
+
       </header>
   )
 }
