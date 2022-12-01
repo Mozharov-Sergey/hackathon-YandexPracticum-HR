ba41da84e857557276c9e9922a19655df27e098d
Mozharov Sergey <mozharov-ss@yandex.ru>
2022-12-01 15:04:02 +0300

    feature: Добавить анимацию переворота карточек

--- hackathon-landing-page/src/components/Courses/CoursesCard.js
+++ hackathon-landing-page/src/components/Courses/CoursesCard.js
@@ -12,16 +12,29 @@ export default function CoursesCard({ item }) {
   }
 
   return (
-    <div className="courses__card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      {!isHover && (
-        <>
-          <h3 className="courses__card-title">{item.title}</h3>
-          <img className="courses__card-image" src={item.image} alt="Программирование"></img>
-          <p className="courses__card-substring">{item.substring}</p>
-        </>
-      )}
+    // <div className="courses__card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    //   {!isHover && (
+    //     <>
+    //       <h3 className="courses__card-title">{item.title}</h3>
+    //       <img className="courses__card-image" src={item.image} alt="Программирование"></img>
+    //       <p className="courses__card-substring">{item.substring}</p>
+    //     </>
+    //   )}
 
-      {isHover && <p className="courses__card-substring_hover">{item.hoverText}</p>}
+    //   {isHover && <p className="courses__card-substring_hover">{item.hoverText}</p>}
+    // </div>
+
+    <div className="courses__card-container">
+      <div className="courses__card courses__card__front">
+        <h3 className="courses__card-title">{item.title}</h3>
+        <img className="courses__card-image" src={item.image} alt="Программирование"></img>
+        <p className="courses__card-substring">{item.substring}</p>
+      </div>
+
+      <div className="courses__card courses__card__back">
+        <p className="courses__card-back-text">{item.hoverText}</p>
+      </div>
+
     </div>
   );
 }
